7638ee076937cd73acacbd8fb6976bc7c0b66c34
Joshua Krupp <jhkrupp@umass.edu>
2021-07-06 10:51:56 -0500

    Added controller with add and delete functionality

--- statusWatch/main.go
+++ statusWatch/main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
-	"context"
+	//"context"
+	//"reflect"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	coreV1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/cache"
 )
+
+func onPodAdd(obj interface{}){
+	pod := obj.(*coreV1.Pod)
+	fmt.Printf("New pod added with image %s\n", pod.Status.ContainerStatuses[0].Image)
+}
+
+func onPodDelete(obj interface{}){
+	pod := obj.(*coreV1.Pod)
+	fmt.Printf("Pod with image %s was deleted", pod.Status.ContainerStatuses)
+}
+/*
+func onPodUpdate(old, new interface{}) {
+	oldPod = old.(*coreV1.Pod)
+	newPod = new.(*coreV1.Pod)
+	fmt.Printf("Pod originally had image %s, but was updated with image %s", oldPod.Status.ContainerStatuses, newPod.Status.ContainerStatuses)
+}
+*/
+func newPodInformer(clientset *kubernetes.Clientset, ns string) {
+	/*if ns == "" {
+		factory := informers.NewSharedInformerFactory(clientset, 0)
+	} else {
+		factory := informers.NewSharedInformerFactory(clientset, 0)
+	}*/
+	factory := informers.NewSharedInformerFactory(clientset, 1000000000)
+	informer := factory.Core().V1().Pods().Informer()
+	fmt.Println("Controller has started")
+	stopper := make(chan struct{})
+	defer close(stopper)
+	defer runtime.HandleCrash()
+	defer fmt.Println("Controller has stopped")
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+        AddFunc : onPodAdd,
+        DeleteFunc: onPodDelete,
+        //UpdateFunc: onPodUpdate,
+    })
+	go informer.Run(stopper)
+	if !cache.WaitForCacheSync(stopper, informer.HasSynced) {
+        runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+        return
+    }
+    <-stopper
+}
 
 func main() {
 	var ns string
@@ -28,15 +73,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	pods, err := clientset.CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		log.Fatalln("failed to get pods:", err)
-	} else if len(pods.Items) == 0 {
-		fmt.Printf("Either no pods exist in %s, or %s namespace does not exist", ns, ns)
-	} else {
-		fmt.Println("Pods:\n")
-		for _, pod := range pods.Items {
-			fmt.Printf("%s\n", pod.GetName())
-		}
-	}
+	newPodInformer(clientset, ns)
 }
